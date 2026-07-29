3be4e3711cbe8f6885eed4ce7b2da86ad38d02f3
ADEBOTE-DANIEL <dannyadebote16@gmail.com>
2026-07-29 22:02:06 +0100

    switch email provider to brevo smtp

--- danieladebote/settings.py
+++ danieladebote/settings.py
@@ -151,13 +151,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 30
 
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('BREVO_EMAIL')
+EMAIL_HOST_PASSWORD = config('BREVO_SMTP_KEY')
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
