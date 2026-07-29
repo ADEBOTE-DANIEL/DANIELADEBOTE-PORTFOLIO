4f47d5d35cd28e08a954f841e421caae18ead964
ADEBOTE-DANIEL <dannyadebote16@gmail.com>
2026-07-29 19:18:01 +0100

    Prepare final deployment settings

--- danieladebote/settings.py
+++ danieladebote/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5qq=(ekcn_hlpb391%6^+h1u(m6gh@(loc&4dmd%awj3h745jm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'danieladebote-portfolio.onrender.com',
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
